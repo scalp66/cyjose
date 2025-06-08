e33f15cc8c2b970450a7a2738f57628205b77804
scalp66 <scalp66@live.fr>
2025-06-08 14:57:53 +0200

    Fix: Correction de l'App ID pour OneSignal

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,10 @@
 // Fichier : public/service-worker.js
 
 // On importe le script de OneSignal au tout début. C'est la ligne la plus importante.
+// Elle permet à notre unique service worker de prendre en charge les notifications.
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 
-// Pour l'instant, on ne met rien d'autre.
-// Plus tard, on pourra ajouter ici la logique de mise en cache pour le mode hors-ligne.
+// Pour l'instant, on ne met rien d'autre dans ce fichier.
+// Plus tard, une fois que tout fonctionnera parfaitement, on pourra ajouter ici
+// la logique pour la mise en cache des fichiers pour le mode hors-ligne.
+// Garder ce fichier simple maintenant nous aide à trouver le problème.
