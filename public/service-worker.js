7bcbd5834da8d1caa82b512c57eab6a5f10b4387
scalp66 <scalp66@live.fr>
2025-06-08 13:53:26 +0200

    Fix: Unification des Service Workers pour OneSignal

---
+++ public/service-worker.js
@@ -0,0 +1,7 @@
+// Fichier : public/service-worker.js
+
+// On importe le script de OneSignal au tout début. C'est la ligne la plus importante.
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+
+// Pour l'instant, on ne met rien d'autre.
+// Plus tard, on pourra ajouter ici la logique de mise en cache pour le mode hors-ligne.
